ec8fe831a339ae1388812d32d6b7b9ca41619ed3
autowattage <pipandw@gmail.com>
2026-08-11 13:12:19 -0400

    not sure why its not working but weball

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
-// console.log("Hello via Bun!");
 import { App } from "@slack/bolt";
 
-const app = new App({
-  
+console.log({
+  slackToken: !!process.env.SLACK_TOKEN,
+  slackAppToken: !!process.env.SLACK_APP_TOKEN,
 });
+
+const app = new App({
+  token: process.env.SLACK_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+app.message(async (event) => {
+    if (event.payload.subtype) return;
+    if (event.payload.user !== 'U0BP2G0BZFH') return;
+
+    await event.say("mrauwwwww");
+})
 
 await app.start();
